c2928cdc83902dbda874f28cd477924b2416179e
Álvaro da Cunha Cavalcanti Pinto Jordão <alvaro.2021104553@unicap.br>
2024-09-11 13:12:11 -0300

    add home page

---
+++ src/pages/login.js
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+      <div>
+        <h1>Página Principal</h1>
+      </div>
+    )
+  }
+  
+  export default Login
---
+++ src/pages/receitas.js
@@ -0,0 +1,9 @@
+const Receitas = () => {
+    return (
+      <div>
+        <h1>Página Principal</h1>
+      </div>
+    )
+  }
+  
+  export default Receitas
